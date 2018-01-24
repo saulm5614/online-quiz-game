59034395359d46b85cfb3efde1bae4846a8afb54
Web Developer <saulm5601@gmail.com>
2018-01-24 14:49:43 -0800

    Quiz fully functional but not finished

---
+++ Quiz/quiz-backend/quiz-backend/Controllers/QuestionsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace quiz_backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/questions")]
+    public class QuestionsController : Controller
+    {
+        private readonly QuizContext _context;
+
+        public QuestionsController(QuizContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Models.Question> Get()
+        {
+            return _context.Questions;
+        }
+
+        [HttpGet("{quizId}")]
+        public IEnumerable<Models.Question> Get([FromRoute] int quizId)
+        {
+            return _context.Questions.Where(q => q.QuizId == quizId);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Models.Question question)
+        {
+            var quiz = _context.Quiz.SingleOrDefault(q => q.ID == question.QuizId);
+            if (quiz == null)
+                return NotFound();
+
+            _context.Questions.Add(question);
+            await _context.SaveChangesAsync();
+            return Ok(question);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody]Models.Question question)
+        {
+            if (id != question.ID)
+                return BadRequest();
+
+            await _context.SaveChangesAsync();
+            _context.Entry(question).State = EntityState.Modified;
+
+            return Ok(question);
+        }
+    }
+}
